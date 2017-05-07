5eab95bf47f9913dce905af7575dc2649bbf98aa
Joan Karadimov <joan@profuzdigital.com>
2017-05-08 01:31:17 +0300

    Implement a YamlModule prototype and use it
    This effectively inlines broccoli-json-module and broccoli-yaml

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 /* jshint node: true */
 'use strict';
 
-var JsonModule = require('broccoli-json-module');
-var YamlToJson = require('broccoli-yaml');
+var Filter = require('broccoli-filter');
+var Yaml = require('js-yaml');
+var merge = require('lodash.merge');
+
+YamlModule.prototype = Object.create(Filter.prototype);
+YamlModule.prototype.constructor = YamlModule;
+YamlModule.prototype.extensions = ['yaml', 'yml'];
+YamlModule.prototype.targetExtension = 'js';
+
+function YamlModule(inputTree, options) {
+  if (!(this instanceof YamlModule)) {
+    return new YamlModule(inputTree, options);
+  }
+
+  this.options = merge({ space: 2 }, options);
+
+  Filter.call(this, inputTree, {
+    annotation: this.options.annotation
+  });
+}
+
+YamlModule.prototype.processString = function (string) {
+  return "export default " + JSON.stringify(Yaml.safeLoad(string), null, this.options.space) + ";";
+}
 
 module.exports = {
   name: 'ember-cli-yaml-module',
 
   treeForApp: function() {
-    return JsonModule(YamlToJson(this.app.trees.app));
+    return YamlModule(this.app.trees.app);
   },
 
   treeForTestSupport: function() {
-    return JsonModule(YamlToJson(this.app.trees.tests));
+    return YamlModule(this.app.trees.tests);
   }
 };
